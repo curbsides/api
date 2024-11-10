87ad12f63bf694d7d0dacf215f55e77e060cd5f5
Thaeriem <kauly.cs@gmail.com>
2024-11-10 06:31:16 -0500

    fix cors

--- app/server/app.py
+++ app/server/app.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes.loc_routes import router as loc_router
 from .routes.img_routes import router as img_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(loc_router, prefix="/loc")
 app.include_router(img_router, prefix="/img")
